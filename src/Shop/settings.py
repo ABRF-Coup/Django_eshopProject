4d4b65040698930f89d2f647915df3e0eab5222c
ABRF-Coup <adafrancine2004@gmail.com>
2024-07-15 22:54:22 +0200

    Creation modele utilisateur et migrations

--- src/Shop/settings.py
+++ src/Shop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'store',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
